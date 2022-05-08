70dc70203d3c3fefd7e635df1ea0de65e0b3ba27
Nothdurftm17 <97467294+Nothdurftm17@users.noreply.github.com>
2022-05-08 11:30:18 -0400

    added a longest substring solution

--- script.js
+++ script.js
@@ -20,4 +20,52 @@ var maxProfit = function(prices) {
 
     return "Max Profit: " + mProfit
 }
-console.log(maxProfit([3,4,1,4,7]))
+// console.log(maxProfit([3,4,1,4,7]))
+
+
+// go through each character of a string 
+// there are no duplicates allowed in substring 
+// check if there are any sub strings in the string 
+// store substring as maxSubstring and continue through string 
+// compare current maxSubString to the potential substrings that follow
+var lengthOfLongestSubstring = function(s) {
+    var maxSub = 0;
+    var sub = "";
+    var l = 0;
+
+    for(var l = 0; l < s.length;l++){
+        for(var r = l; r < s.length; r++){
+            if(!sub.includes(s[r])){
+                sub += s[r]
+                console.log(sub)
+                maxSub = Math.max(maxSub, sub.length)
+            }
+            else{
+                sub = ""
+                break
+            }
+        }
+    }
+    
+    return "Longest Substring: ", maxSub;
+};
+console.log(lengthOfLongestSubstring("dvdf"))
+
+// var lengthOfLongestSubstring = function(s) {
+//     const sub = new Set();
+//     l = 0
+//     res = 0
+
+//     for (var r = 0; r < s.length; r++){
+//         console.log(sub)
+//         if (sub.includes(s[r])){
+//             sub.delete(s[l])
+//             l++
+//         }
+//         sub.add(s[r])
+//         res = Math.max(res, r - l)
+//     }
+//     return res
+// }
+
+// console.log(lengthOfLongestSubstring("abcabcbb"))
